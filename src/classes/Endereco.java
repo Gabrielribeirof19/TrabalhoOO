04f147ec504c5572c032183331f24b50567ffa97
Gabriel De Souza <gabrielribeirof69@gmail.com>
2021-09-10 03:11:53 -0300

    Creating Endereco class

--- src/classes/Endereco.java
+++ src/classes/Endereco.java
@@ -1,1 +1,36 @@
 package classes;
+import javax.print.DocFlavor.STRING;
+
+
+public class Endereco{
+    private int CEP;
+    private String bairro;
+    private String complemento;
+    private String endereco;
+
+    public int getCEP(){
+        return CEP;
+    }
+    public void setCEP(int cep){
+        this.CEP = cep;
+    }
+    public String getBairro(){
+        return bairro;
+    }
+    public void setBairoo(String Bairro){
+        this.bairro = Bairro;
+    }
+    public String getComplemento(){
+        return complemento;
+    } 
+    public void setComplemento(String compl){
+        this.complemento = compl;
+    }
+    public String getEndereco(){
+        return endereco;
+    }
+    public void setEndereco(String end){
+        this.endereco = end;
+    }
+
+}
